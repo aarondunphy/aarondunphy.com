a8f83ca125cd444e2697f0469c7bd01a62c66411
Aaron <aaronjohndunphy@gmail.com>
2022-02-07 12:33:51 +0000

    Content update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: `Aaron Dunphy`,
-    description: `Full Stack Developer based in Leigh-on-Sea, UK. Laravel & JavaScript.`,
+    title: `Aaron Dunphy | Freelance Web Developer | Developer Mentor`,
+    description: `Freelance Web Developer based in Leigh-on-Sea, Essex. On a mission to help people start and build a career in Web Development.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-LCQ7QC3KD1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "aarondunphy.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
