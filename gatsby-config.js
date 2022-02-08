e704bbf64387b6d1aefdacb8b6b27e3a085d83ae
Aaron <aaronjohndunphy@gmail.com>
2022-02-08 15:10:08 +0000

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
