77f4092264dd1f13ce7dac565f07717ba6a88c60
Aaron Dunphy <aaronjohndunphy@gmail.com>
2023-01-26 18:31:52 +0000

    Remove blog from website and simplify

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
 import React from "react"
 import Layout from "../src/components/layout"
 import Seo from "../src/components/seo"
-import BlogCard from "../src/components/BlogCard/BlogCard"
-import Heading from "../src/components/Heading"
-import { getAllPosts } from "../lib/blog"
-import { PostFrontmatter } from "../src/types"
 
-interface Props {
-  posts: PostFrontmatter[]
-}
-
-const Index = ({ posts }: Props) => {
-  const Posts = () => {
-    return (
-      <>
-        {posts.map((post, index) => (
-          <div key={index} style={{ marginBottom: 40 }}>
-            <BlogCard
-              title={post.frontmatter.title}
-              slug={post.frontmatter.slug}
-              date={post.frontmatter.date}
-            />
-          </div>
-        ))}
-      </>
-    )
-  }
-
+const Index = () => {
   return (
     <Layout>
       <Seo
@@ -45,25 +21,9 @@ const Index = ({ posts }: Props) => {
             Front End Developer based in Leigh-on-Sea, UK
           </h1>
         </div>
-
-        <div className="posts">
-          <Heading level={2}>Blog Posts</Heading>
-          <div className="posts__cardsContainer">
-            <Posts />
-          </div>
-        </div>
       </div>
     </Layout>
   )
 }
 
 export default Index
-
-export async function getStaticProps() {
-  const posts = getAllPosts()
-  return {
-    props: {
-      posts,
-    },
-  }
-}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,77 +19,10 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="container nav">
-        <button
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          type="button"
-          className="nav__menuButton"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-        >
-          {isMenuOpen && (
-            <>
-              <span className="sr-only">Close main menu</span>
-              <svg
-                className="nav__menuButton-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </>
-          )}
-          {!isMenuOpen && (
-            <>
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="nav__menuButton-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </>
-          )}
-        </button>
-
         <div className="nav__container">
           <Link href="/" className="logo">
             Aaron Dunphy
           </Link>
-          <div
-            className={`nav__links-container ${
-              isMenuOpen ? "nav__links-container--open" : ""
-            }`}
-          >
-            <Link href="/" className="nav__link">
-              Home
-            </Link>
-            <Link href="/about" className="nav__link">
-              About
-            </Link>
-            <Link href="/blog" className="nav__link">
-              Blog
-            </Link>
-            <Link href="/uses" className="nav__link">
-              Uses
-            </Link>
-          </div>
         </div>
       </nav>
     </header>
